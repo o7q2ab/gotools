a8c802df67f5fe772c6151030a07b835203f3e0b
o7q2ab <o7q2ab@gmail.com>
2023-04-02 12:09:16 +0300

    cmd/srv: require a port number

--- cmd/srv/main.go
+++ cmd/srv/main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("Listening on port 8888.")
-	if err := http.ListenAndServe(":8888", http.HandlerFunc(
+	if len(os.Args) != 2 {
+		fmt.Printf("error: run '%s <port number>'\n", os.Args[0])
+		os.Exit(1)
+	}
+	port := os.Args[1]
+	if _, err := strconv.Atoi(port); err != nil {
+		fmt.Println("error: port must be a number")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Listening on port %s.\n", port)
+	if err := http.ListenAndServe(":"+port, http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			fmt.Println(r.Method, r.RequestURI)
 			w.WriteHeader(http.StatusOK)
