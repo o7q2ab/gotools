1ed796b1a6feab26088c29e24131164892680606
o7q2ab <o7q2ab@gmail.com>
2023-08-08 20:57:21 +0300

    cmd/goxm: print size, file mode and info about module

--- cmd/goxm/main.go
+++ cmd/goxm/main.go
@@ -6,6 +6,15 @@ import (
 	"os"
 )
 
+const header = `%s
+
+  - Package: %s
+  - Module:  %s %s (%s)
+  - Version: %s
+  - Size:    %d bytes
+  - Mode:    %s
+`
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Printf("usage: '%s <path>'\n", os.Args[0])
@@ -18,20 +27,27 @@ func main() {
 }
 
 func run(path string) error {
+	stat, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
 	info, err := buildinfo.ReadFile(path)
 	if err != nil {
 		return err
 	}
-	fmt.Println(path)
-	fmt.Printf("  %s\n", info.GoVersion)
-	fmt.Printf("  %s\n", info.Path)
-	fmt.Printf("  Dependencies:\n")
-	for _, d := range info.Deps {
-		fmt.Printf("    %s (%s)\n", d.Path, d.Version)
-	}
-	fmt.Printf("  Build settings:\n")
+	fmt.Printf(
+		header, path, info.Path, info.Main.Path, info.Main.Version, info.Main.Sum,
+		info.GoVersion, stat.Size(), stat.Mode(),
+	)
+
+	fmt.Printf("\n  - Build settings:\n")
 	for _, s := range info.Settings {
 		fmt.Printf("    %s=%s\n", s.Key, s.Value)
+	}
+
+	fmt.Printf("\n  - Dependencies [%d]:\n", len(info.Deps))
+	for _, d := range info.Deps {
+		fmt.Printf("    %s %s\n", d.Path, d.Version)
 	}
 	return nil
 }
